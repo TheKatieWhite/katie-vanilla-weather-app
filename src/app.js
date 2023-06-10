325208df732a1af4b741553058e64f999975e419
TheKatieWhite <132315102+TheKatieWhite@users.noreply.github.com>
2023-06-10 12:16:24 +0900

    Show current weather info

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#tempNow");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = `<strong>${response.data.name}</strong>`;
+  let conditionElement = document.querySelector("#condition");
+  conditionElement.innerHTML = response.data.weather[0].main;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "8402ccd9e55983fce71eeeaa1d2bd1fc";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
