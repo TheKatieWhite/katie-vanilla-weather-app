9d74d1dd3d1108c2e5002dc4d4e9771895784549
TheKatieWhite <132315102+TheKatieWhite@users.noreply.github.com>
2023-06-10 12:39:14 +0900

    Update time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = weekDays[date.getDay()];
+  return `Updated ${day} ${hours}:${minutes}, `;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#tempNow");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -9,6 +32,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dayTimeElement = document.querySelector("#currentDayTime");
+  dayTimeElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "8402ccd9e55983fce71eeeaa1d2bd1fc";
