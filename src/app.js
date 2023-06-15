6d5bd2f19065bc3d96cc46f1dd45ff75b2d2cd08
TheKatieWhite <132315102+TheKatieWhite@users.noreply.github.com>
2023-06-15 15:48:02 +0900

    Removed cel and removed local weather button

--- src/app.js
+++ src/app.js
@@ -72,10 +72,9 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "8402ccd9e55983fce71eeeaa1d2bd1fc";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -101,15 +100,12 @@ function displayTemperature(response) {
 
   getForecast(response.data.coord);
 
-  https: fLink.innerHTML = "<a style='color:#3ebdff'> °F </a>";
-  cLink.innerHTML = "<a style='color: blue'> °C </a>";
-
   celTemp = response.data.main.temp;
 }
 
 function search(city) {
   let apiKey = "8402ccd9e55983fce71eeeaa1d2bd1fc";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
@@ -119,31 +115,7 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-function showFahTemp(event) {
-  event.preventDefault();
-  let fahTemp = (celTemp * 9) / 5 + 32;
-  let temperatureElement = document.querySelector("#tempNow");
-  temperatureElement.innerHTML = Math.round(fahTemp);
-  fLink.innerHTML = "<a style='color:blue'> °F </a>";
-  cLink.innerHTML = "<a style='color:#3ebdff'> °C </a>";
-}
-
-function showCelTemp(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#tempNow");
-  temperatureElement.innerHTML = Math.round(celTemp);
-  fLink.innerHTML = "<a style='color:#3ebdff'> °F </a>";
-  cLink.innerHTML = "<a style='color: blue'> °C </a>";
-}
 search("kettering");
-
-let celTemp = null;
 
 let searchWeather = document.querySelector("#enterCity");
 searchWeather.addEventListener("submit", handleSubmit);
-
-let fLink = document.querySelector("#fah");
-fLink.addEventListener("click", showFahTemp);
-
-let cLink = document.querySelector("#cel");
-cLink.addEventListener("click", showCelTemp);
